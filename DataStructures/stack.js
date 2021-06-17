02b0742eb88f4a90ab07a617d54b5930aa73da03
jeongha-joo <jha970107@gmail.com>
2021-06-18 00:07:43 +0900

    자료구조 연습 stack

---
+++ DataStructures/stack.js
@@ -0,0 +1,26 @@
+class Stack {
+  constructor() {
+    this._arr = [];
+  }
+  push(item) {
+    this._arr.push(item);
+  }
+  pop() {
+    return this._arr.pop();
+  }
+  peek() {
+    return this._arr[this._arr.length - 1];
+  }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.push(5);
+stack.peek();
+stack.pop();
+stack.pop();
+stack.push(6);
+console.log(stack);
